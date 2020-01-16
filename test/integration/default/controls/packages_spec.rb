86e53ab68df5c2f3680f21a3724308014b4b10a5
kjkeane <kjkeane@protonmail.com>
2020-01-16 03:02:32 -0500

    fix(kitchen): update expected permissions

--- test/integration/default/controls/packages_spec.rb
+++ test/integration/default/controls/packages_spec.rb
@@ -4,6 +4,6 @@ control 'Mattermost package' do
   title 'should be installed'
 
   describe directory('/opt/mattermost') do
-    its('mode') { should cmp '0644' }
+    its('mode') { should cmp '0755' }
   end
 end
